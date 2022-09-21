2c5423602c3a432eaf39ff98e90395f53fbbf28d
Sam Rutcovschi <samuil.rutcovschi@gmail.com>
2022-09-21 09:44:45 +0300

    add button reset

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.lab1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +29,17 @@ class MainActivity : AppCompatActivity() {
                 if(bmi > 18.5 && bmi < 24.9) text_result2.text = "Normal weight"
                 if(bmi > 25.0 && bmi < 29.9) text_result2.text = "Overweight"
                 if(bmi > 30) text_result2.text = "Obesity"
+
+            }
+
+            button_reset.setOnClickListener {
+                if(input_height.text?.length != 0
+                    || input_weight.text?.length != 0)
+                {
+
+                    input_weight.setText("")
+                    input_height.setText("")
+                }
             }
         }
 
