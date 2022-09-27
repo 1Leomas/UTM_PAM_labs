aaae7a8a718143d0a8fd9821019f9a551740af0b
Sam Rutcovschi <samuil.rutcovschi@gmail.com>
2022-09-27 23:49:08 +0300

    lab2 flutter change design of app

--- lab2_flutter/lib/main.dart
+++ lab2_flutter/lib/main.dart
@@ -5,12 +5,19 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'BMI Calculator';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepPurpleAccent,
+          foregroundColor: Colors.white,
+          shadowColor: Colors.grey,
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
         body: const MyStatefulWidget(),
@@ -31,9 +38,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   double h = 0;
   double w = 0;
-  double result = 1;
+  double result = 0.0;
 
   String bmi = '';
+  String bmi_status = '';
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +53,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           Padding(
             padding: const EdgeInsets.fromLTRB(50, 100, 50, 0),
             child: TextFormField(
+              style: const TextStyle (fontSize: 20),
               decoration: const InputDecoration(
                 hintText: 'Enter your height',
               ),
@@ -57,7 +66,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               },
               onChanged: (value){
                 setState(() {
-                  h= double.parse(value);
+                  h = double.parse(value)/100;
                 });
               },
             ),
@@ -65,6 +74,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           Padding(
             padding: const EdgeInsets.fromLTRB(50, 16, 50, 0),
             child: TextFormField(
+              style: const TextStyle (fontSize: 20),
               decoration: const InputDecoration(
                 hintText: 'Enter your weight',
               ),
@@ -75,6 +85,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 }
                 return null;
               },
+
               onChanged: (value){
                 setState(() {
                   w = double.parse(value);
@@ -82,23 +93,71 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               },
             ),
           ),
+          Flexible(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            result = w/(h*h);
+                            bmi = result.toStringAsFixed(1);
+
+                            if(result < 18.5) bmi_status = 'Underweight';
+                            if(result > 18.5 && result < 24.9) bmi_status = 'Normal weight';
+                            if(result > 25.0 && result < 29.9) bmi_status = 'Overweight';
+                            if(result > 30) bmi_status = 'Obesity';
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                      ),
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState?.reset();
+                          setState(() {
+                            bmi = '';
+                            bmi_status = '';
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                      ),
+                      child: const Text('Reset'),
+                    ),
+                  ),
+                ],
+              ),
+          ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState!.validate()) {
-                  setState(() {
-                    result = w/(h*h);
-                    bmi = result.toStringAsFixed(1);
-                  });
-                }
-              },
-              child: const Text('Submit'),
+            padding: const EdgeInsets.fromLTRB(0, 26, 0, 0),
+            child: Text(
+              //bmi,
+              bmi = bmi.isNotEmpty ? 'Your BMI is $bmi' : '',
+              //myController.text = (myController.text.isNotEmpty) ? 'Your BMI is ${myController.text}' : '',
+              style: const TextStyle (fontSize: 28),
             ),
           ),
-          Text(bmi)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 26, 0, 0),
+            child: Text(
+              //bmi_status,
+              bmi_status = bmi.isNotEmpty ? bmi_status : '',
+              //myController.text = (myController.text.isNotEmpty) ? bmi_status: '',
+              style: const TextStyle (fontSize: 28),
+            ),
+          ),
         ],
       ),
     );
