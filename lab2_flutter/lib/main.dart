50a722eeec0396297dc249e6d6ca7c564318874c
Sam Rutcovschi <samuil.rutcovschi@gmail.com>
2022-09-28 12:05:20 +0300

    lab2 flutter add animation

--- lab2_flutter/lib/main.dart
+++ lab2_flutter/lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'BMI Calculator';
+  static const String _title = 'Laboratorul nr.2';
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +43,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   String bmi = '';
   String bmi_status = '';
 
+  bool pressed = false;
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -50,8 +51,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       child: Column(
         //crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.fromLTRB(50, 50, 50, 0),
+            child: Text(
+              'BMI Calculator',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(50, 100, 50, 0),
+            padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
             child: TextFormField(
               style: const TextStyle (fontSize: 20),
               decoration: const InputDecoration(
@@ -110,13 +122,15 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                             if(result > 18.5 && result < 24.9) bmi_status = 'Normal weight';
                             if(result > 25.0 && result < 29.9) bmi_status = 'Overweight';
                             if(result > 30) bmi_status = 'Obesity';
+
+                            pressed = true;
                           });
                         }
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.deepPurpleAccent,
                       ),
-                      child: const Text('Submit'),
+                      child: const Text('Calculate'),
                     ),
                   ),
                   Padding(
@@ -128,6 +142,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                           setState(() {
                             bmi = '';
                             bmi_status = '';
+
+                            pressed = false;
                           });
                         }
                       },
@@ -142,16 +158,41 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 26, 0, 0),
-            child: Text( bmi.isNotEmpty ? 'Your BMI is $bmi' : '',
-              style: const TextStyle (fontSize: 28),
+            child: AnimatedContainer(
+              transform: pressed ? Matrix4.diagonal3Values(1, 1, 1) : Matrix4.diagonal3Values(1, 0.1, 0.1),
+              width: 280,
+              height: 150,
+              //width: pressed ? 260.0 : 0.0,
+              //height: pressed ? 150.0 : 0.0,
+              //color: pressed ? Colors.deepPurpleAccent : Colors.white70,
+              alignment: pressed ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: const Duration(seconds: 2),
+              curve: Curves.easeOutCirc,
+              decoration: BoxDecoration(
+                  color: pressed ? Colors.deepPurpleAccent : Colors.white10,
+                  borderRadius: const BorderRadius.all(Radius.circular(10))
+              ),
+              child: Text( bmi.isNotEmpty ? 'Your BMI is $bmi \n\n$bmi_status' : '',
+                style: const TextStyle (fontSize: 28, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
+          ),/*
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 26, 0, 0),
-            child: Text( bmi.isNotEmpty ? bmi_status : '',
-              style: const TextStyle (fontSize: 28),
+            child: AnimatedContainer(
+              width: pressed ? 200.0 : 0.0,
+              height: pressed ? 80.0 : 0.0,
+              color: pressed ? Colors.deepPurpleAccent : Colors.white70,
+              alignment:
+              pressed ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+              child: Text( bmi.isNotEmpty ? bmi_status : '',
+                style: const TextStyle (fontSize: 28),
+              ),
             ),
-          ),
+          ),*/
         ],
       ),
     );
